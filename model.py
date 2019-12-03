a64142e2e752df2fcc88f2f7a7ff61ec54fc0c70
Denisa Roberts <d.roberts@vt.edu>
2019-12-03 15:43:03 -0500

    Build model.

--- model.py
+++ model.py
@@ -9,26 +9,19 @@ from mxnet import gluon
 import gluonnlp as nlp
 
 from gluonnlp.model.seq2seq_encoder_decoder import _get_attention_cell
+from gluonnlp.model.transformer import PositionwiseFFN
 
 _SEQ_LEN = 32
 
-# TODO:
-# -add transformer layers; prolly start with transformer block
+def _position_encoding_init(max_length, dim):
+    """Init the sinusoid position encoding table """
+    position_enc = np.arange(max_length).reshape((-1, 1)) \
+                   / (np.power(10000, (2. / dim) * np.arange(dim).reshape((1, -1))))
+    print(position_enc)
+    position_enc[:, 0::2] = np.sin(position_enc[:, 0::2])  # dim 2i
+    position_enc[:, 1::2] = np.cos(position_enc[:, 1::2])
 
-# be patient with yourself; this is a lot of stuff you havent worked with the transformer before
-# _ yes what I need to look at is the transformer encoder portion of the gluon nlp encoder decoder model.
-# plan: replicate that code here
-# - first try to import classes and use the Encoder class directly - goal how to import
-
-# Ok need to backoff and to understand the transformer encoder and the code
-# start first with the use of the encoder
-# only encoder portion for the alibaba rec in the transformer layer. Just do one transformer layer for now.
-# It consists of initial item and position embeddings; after that: multi-head attention; then add and norm;
-# then a feed forward layer; then add and norm; then the output of this thransformer layer goes into the next
-# leaky rely layers.
-
-# TODO: today - add a multi-head attention cell to the HybridSequential.DONE. Separate from Hyrbid though. DONE
-# TODO: next transformer layer: add and norm
+    return position_enc
 
 
 class Rec(HybridBlock):
@@ -49,9 +42,28 @@ class Rec(HybridBlock):
                                                       num_heads=4,
                                                       use_bias=False)
 
-            # TODO
+            encoding = _position_encoding_init(_SEQ_LEN, 16)
 
+            # TODO: add projection layer: Done
+            self.proj = nn.Dense(units=16,
+                                 use_bias=False,
+                                 bias_initializer='zeros',
+                                 weight_initializer=None,
+                                 flatten=False
+                                 )
+            # self.Leaky_relu_layer = LeakyReLU(0.1)
+            self.drop_out_layer = nn.Dropout(rate=0.1)
+            #
+            # # TODO: add pointwise feedforward network: Done. Use it now on outputs. Done. Recheck after layer complete
+            # # for correct order and additions.
+            self.ffn = PositionwiseFFN(hidden_size=16,
+                                        use_residual=True,
+                                                             dropout=0.1,
+                                                             units=16,
+                                                             weight_initializer=None,
+                                                             bias_initializer='zeros')
 
+            self.layer_norm = nn.LayerNorm(in_channels=16)
 
             self.output = HybridSequential()
             # final leaky rely layers; dimensions were 1024, 512, 256
@@ -63,12 +75,41 @@ class Rec(HybridBlock):
             self.output.add(LeakyReLU(alpha=0.1))
             self.output.add(nn.Dense(1))
 
+
+
+            # TODO: add position embedding; do sin and cos right now but alibaba does something else:
+             # TODO: likely ffn does the positional encoding; check in basetransformer cell where ffn(output) what
+            # will ffn do with the argument if it will combine it with the positional stuff; if yes, then until
+            # I change the type of positional encoding, the sinusoidal stuff is already added from importing
+            # PositionFFN
+            # TODO: add other features
+            # TODO: fix the transformer encoder to be like in the ALIbaba paper
+            # TODO: push omline
+            # TODO: change position encoding like in alibaba paper; likely the entire POsitionFFN class must be changed.
+            # TODO: feed movie data from Google
+
     def hybrid_forward(self, F, x, mask=None):
         x = self.features(x)
-        x, attn_w = self.cell(x, x, x, mask)
-        x = self.output(x)
-        return x
 
+        # attention cell
+        out_x, attn_w = self.cell(x, x, x, mask)
+        # project
+        out_x = self.proj(out_x)
+
+        # dropout as in nmt
+
+        x = self.drop_out_layer(out_x)
+
+        # add and norm
+        x = x + out_x
+        x = self.layer_norm(x)
+
+        # ffn
+        x = self.ffn(x)
+        x = self.output(x)
+
+        return x
+    # TODO: fix the layer norm
 
 # Constructor
 def ali_rec(**kwargs):
