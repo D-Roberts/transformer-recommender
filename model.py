df2a8e10fdf2783411f967094c63b8aa5c1a4286
Denisa Roberts <d.roberts@vt.edu>
2019-12-04 14:12:09 -0500

    First prototype of ali rec net complete

--- model.py
+++ model.py
@@ -2,7 +2,8 @@
 Alibaba transformer based recommender.
 
 """
-
+import mxnet as mx
+import numpy as np
 from mxnet.gluon import nn
 from mxnet.gluon.nn import HybridBlock, HybridSequential, LeakyReLU
 from mxnet import gluon
@@ -12,12 +13,13 @@ from gluonnlp.model.seq2seq_encoder_decoder import _get_attention_cell
 from gluonnlp.model.transformer import PositionwiseFFN
 
 _SEQ_LEN = 32
+_OTHER_LEN = 32
 
 def _position_encoding_init(max_length, dim):
     """Init the sinusoid position encoding table """
     position_enc = np.arange(max_length).reshape((-1, 1)) \
                    / (np.power(10000, (2. / dim) * np.arange(dim).reshape((1, -1))))
-    print(position_enc)
+    # print(position_enc)
     position_enc[:, 0::2] = np.sin(position_enc[:, 0::2])  # dim 2i
     position_enc[:, 1::2] = np.cos(position_enc[:, 1::2])
 
@@ -28,13 +30,15 @@ class Rec(HybridBlock):
     def __init__(self, **kwargs):
         super(Rec, self).__init__(**kwargs)
         with self.name_scope():
+
+            self.otherfeatures = nn.Embedding(input_dim=_OTHER_LEN,
+                                              output_dim=16)
             self.features = HybridSequential()
 
-            # TODO: add other features and positional encoding.
             self.features.add(nn.Embedding(input_dim=_SEQ_LEN,
-                                           output_dim=16))
+                                           output_dim=32))
 
-
+            # Transformer layer
             self.cell = _get_attention_cell('multi_head',
                                                       units=16,
                                                       scaled=True,
@@ -42,9 +46,6 @@ class Rec(HybridBlock):
                                                       num_heads=4,
                                                       use_bias=False)
 
-            encoding = _position_encoding_init(_SEQ_LEN, 16)
-
-            # TODO: add projection layer: Done
             self.proj = nn.Dense(units=16,
                                  use_bias=False,
                                  bias_initializer='zeros',
@@ -53,9 +54,6 @@ class Rec(HybridBlock):
                                  )
             # self.Leaky_relu_layer = LeakyReLU(0.1)
             self.drop_out_layer = nn.Dropout(rate=0.1)
-            #
-            # # TODO: add pointwise feedforward network: Done. Use it now on outputs. Done. Recheck after layer complete
-            # # for correct order and additions.
             self.ffn = PositionwiseFFN(hidden_size=16,
                                         use_residual=True,
                                                              dropout=0.1,
@@ -75,28 +73,56 @@ class Rec(HybridBlock):
             self.output.add(LeakyReLU(alpha=0.1))
             self.output.add(nn.Dense(1))
 
+    def _arange_like(self, F, inputs, axis):
+        """Helper function to generate indices of a range"""
+        if F == mx.ndarray:
+            seq_len = inputs.shape[axis]
+            arange = F.arange(seq_len, dtype=inputs.dtype, ctx=inputs.context)
+        else:
+            input_axis = inputs.slice(begin=(0, 0, 0), end=(1, None, 1)).reshape((-1))
+            zeros = F.zeros_like(input_axis)
+            arange = F.arange(start=0, repeat=1, step=1,
+                              infer_range=True, dtype=inputs.dtype)
+            arange = F.elemwise_add(arange, zeros)
+            # print(arange)
+        return arange
 
 
-            # TODO: add position embedding; do sin and cos right now but alibaba does something else:
-             # TODO: likely ffn does the positional encoding; check in basetransformer cell where ffn(output) what
-            # will ffn do with the argument if it will combine it with the positional stuff; if yes, then until
-            # I change the type of positional encoding, the sinusoidal stuff is already added from importing
-            # PositionFFN
-            # TODO: add other features
             # TODO: fix the transformer encoder to be like in the ALIbaba paper
-            # TODO: push omline
+
+            # TODO: clean up, replace hard_coding dimensions and push online
+
+            # TODO: c add other options (such as valid length and masking etc)
             # TODO: change position encoding like in alibaba paper; likely the entire POsitionFFN class must be changed.
             # TODO: feed movie data from Google
 
-    def hybrid_forward(self, F, x, mask=None):
+    def _get_positional(self, weight_type, max_length, units):
+        if weight_type == 'sinusoidal':
+            encoding = _position_encoding_init(max_length, units)
+
+        # will have the alibaba weight_type as well
+
+        return mx.nd.array(encoding)
+
+
+    def hybrid_forward(self, F, x, x_other, mask=None):
+
+        x1 = self.otherfeatures(x_other)
+
+        steps = self._arange_like(F, x, axis=1)
         x = self.features(x)
+        position_weight = self._get_positional('sinusoidal', 32, 32)
+
+        # add positional embedding
+        positional_embedding = F.Embedding(steps, position_weight, 32, 32)
+        #print(positional_embedding.shape)
+        #print(x.shape)
+        x = F.broadcast_add(x, F.expand_dims(positional_embedding, axis=0))
 
         # attention cell
         out_x, attn_w = self.cell(x, x, x, mask)
         # project
         out_x = self.proj(out_x)
-
-        # dropout as in nmt
 
         x = self.drop_out_layer(out_x)
 
@@ -106,10 +132,15 @@ class Rec(HybridBlock):
 
         # ffn
         x = self.ffn(x)
+
+        # concat other features with transformer representations
+        x = mx.ndarray.concat(x, x1)
+
+        # Leakyrelu final layers
         x = self.output(x)
 
         return x
-    # TODO: fix the layer norm
+
 
 # Constructor
 def ali_rec(**kwargs):
--- transformer.py
+++ transformer.py
@@ -22,19 +22,11 @@ _SEQ_LEN = 32
 _BATCH = 1
 ctx = mx.cpu()
 
-# TODO:
-# - READ ALIBABA PAPER TO UNDERSTAND THE MODEL
-# - Code the model class with a test with random generated data similar to quo vadis. This will be the train file.
-# - GET THE DATA FROM GOOGLE MOVIE REC AND GET IT READY FOR THE MODEL.
-
-
-# TODO (small):
-# - set up the train file - bare bones (see quo vadis)(DONE); start with artificially generated data (done)
-# - next add embedding layer; and last three leaky relus layers
 
 def generate_sample():
     """Generate X and y."""
-    X = mx.random.uniform(shape=(100, 32))
+    X = mx.random.uniform(shape=(100, 64))
+
     y = mx.random.uniform(shape=(100, 1))
     y = y > 0.5
     # Data loader for a simple MLP to start with
@@ -44,6 +36,8 @@ def generate_sample():
 def train():
     train_data = generate_sample()
     optimizer = mx.optimizer.Adam()
+    # loss is here for Binary outcome
+    # paper says sigmoid
     loss = gluon.loss.SigmoidBinaryCrossEntropyLoss()
 
     net = model.Rec()
@@ -59,7 +53,7 @@ def train():
 
         for x, y in train_data:
             with ag.record():
-                output = net(x)
+                output = net(x[:,:32], x[:, 32:])
                 l = loss(output, y).sum()
 
         l.backward()
@@ -67,8 +61,10 @@ def train():
         train_metric.update(y, output)
 
 
+
 def main():
     train()
+
 
 if __name__ == '__main__':
     main()
